bc8abdcb760f00f829dc2ee2bab2d3f0d9ae74b9
fullwehrer <109440414+fullwehrer@users.noreply.github.com>
2022-08-23 00:56:17 +0200

    check if entry exists

--- filehandling.py
+++ filehandling.py
@@ -31,11 +31,17 @@ def createentry(savefile, year, month, day, first, blood, pain, painkiller):
 
 def orderbydate(savefile):
     filedata=pd.read_csv(savefile, header=None)
-    print(filedata)
     filedata.sort_values([filedata.columns[0],filedata.columns[1],filedata.columns[2]], axis=0, ascending=[True,True, True], inplace=True)
     filedata.to_csv(savefile, index=False, header=False)
 
-# def dateoflastentry(savefile, year, month, day):
-#     filedata=pd.read_csv(savefile, header=None)
+def checkentryexists(savefile, year, month, day):
+    with open(savefile, 'r', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0]==year and row[1]==month and row[2]== day:
+                return True
+            else:
+                return False
+    f.close()
 
 # def fillzeros(date):
